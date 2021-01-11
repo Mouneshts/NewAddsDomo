469308371a10804fa6da0efe3347c1b4ea4f5dfe
Mounesh <mounitsmurthy@gmail.com>
2021-01-11 22:03:23 +0530

    Add facebook packages and method or funcations to the project

--- NewAddsDomo/Controllers/HomeController.cs
+++ NewAddsDomo/Controllers/HomeController.cs
@@ -8,6 +8,8 @@ using System.Data.SqlClient;
 using System.Configuration;
 using System.IO;
 using NewAddsDomo.Models;
+using System.Web.Security;
+using Facebook;
 
 namespace NewAddsDemo.Controllers
 {
@@ -65,5 +67,105 @@ namespace NewAddsDemo.Controllers
 
             return View();
         }
+
+        private Uri RediredtUri
+        {
+
+            get
+            {
+
+                var uriBuilder = new UriBuilder(Request.Url);
+
+                uriBuilder.Query = null;
+
+                uriBuilder.Fragment = null;
+
+                uriBuilder.Path = Url.Action("FacebookCallback");
+
+                return uriBuilder.Uri;
+
+            }
+
+        }
+
+        [AllowAnonymous]
+
+        public ActionResult Facebook()
+
+        {
+
+            var fb = new FacebookClient();
+
+            var loginUrl = fb.GetLoginUrl(new
+
+            {
+
+
+
+
+                client_id = "Your App ID",
+
+                client_secret = "Your App Secret key",
+
+                redirect_uri = RediredtUri.AbsoluteUri,
+
+                response_type = "code",
+
+                scope = "email"
+
+
+
+            });
+
+            return Redirect(loginUrl.AbsoluteUri);
+
+        }
+
+
+
+
+        public ActionResult FacebookCallback(string code)
+        {
+
+            var fb = new FacebookClient();
+
+            dynamic result = fb.Post("oauth/access_token", new
+
+            {
+
+                client_id = "Your App ID",
+
+                client_secret = "Your App Secret key",
+
+                redirect_uri = RediredtUri.AbsoluteUri,
+
+                code = code
+
+            });
+
+            var accessToken = result.access_token;
+
+            Session["AccessToken"] = accessToken;
+
+            fb.AccessToken = accessToken;
+
+            dynamic me = fb.Get("me?fields=link,first_name,currency,last_name,email,gender,locale,timezone,verified,age_range");
+
+            string email = me.email;
+
+            TempData["email"] = me.email;
+
+            TempData["first_name"] = me.first_name;
+
+            TempData["lastname"] = me.last_name;
+
+
+            FormsAuthentication.SetAuthCookie(email, false);
+
+            return RedirectToAction("Index", "Home");
+
+        }
+
+
     }
 }
